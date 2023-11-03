74e7934e9050d6b85d6d36df896ed17246f4ca27
rajat jana <rajatjana2024@gmail.com>
2023-11-03 23:47:13 +0530

    added url handling routes like youtube

--- dir03server/index.js
+++ dir03server/index.js
@@ -1,22 +1,32 @@
 const http = require("http");
 const fs = require("fs");
+var url = require('url');
+
 const PORT = 8000;
 
 const myServer = http.createServer((req, res) => {
   console.log("new req recived");
+
+  //url parsing
+  const myUrl = url.parse(req.url,true);
+  console.log(myUrl.query);
+  
   var log = `\n${Date.now()} ${req.url}`;
   fs.appendFile("./log.txt", log, () => {
-    switch (req.url) {
+    switch (myUrl.pathname) {
       case "/":
-        res.end("welcome to home");
+        res.end("welcome to home\n"+myUrl.query.name+"  "+myUrl.query.id);
         break;
 
       case "/about":
         res.end("i am rajat");
         break;
 
+      case "/results":
+       res.end(`your search rersults for ${myUrl.query.search_query} are >>>`);
+
       default:
-        res.end("it 404 error");
+        res.end("it 404 error\n"+myUrl.query.name+"  "+myUrl.query.id);
         break;
     }
   });
