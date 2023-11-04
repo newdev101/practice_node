f37350ae292662eac16215206ea206dc8dda1679
rajat jana <rajatjana2024@gmail.com>
2023-11-04 09:37:55 +0530

    using handler pointof view

--- dir03server/index.js
+++ dir03server/index.js
@@ -1,21 +1,23 @@
 const http = require("http");
 const fs = require("fs");
-var url = require('url');
+var url = require("url");
+const exp = require("constants");
+
 
 const PORT = 8000;
 
-const myServer = http.createServer((req, res) => {
+function myHandler(req, res) {
   console.log("new req recived");
 
   //url parsing
-  const myUrl = url.parse(req.url,true);
+  const myUrl = url.parse(req.url, true);
   console.log(myUrl.query);
-  
+
   var log = `\n${Date.now()} ${req.url}`;
   fs.appendFile("./log.txt", log, () => {
     switch (myUrl.pathname) {
       case "/":
-        res.end("welcome to home\n"+myUrl.query.name+"  "+myUrl.query.id);
+        res.end("welcome to home\n" + myUrl.query.name + "  " + myUrl.query.id);
         break;
 
       case "/about":
@@ -23,13 +25,15 @@ const myServer = http.createServer((req, res) => {
         break;
 
       case "/results":
-       res.end(`your search rersults for ${myUrl.query.search_query} are >>>`);
+        res.end(`your search rersults for ${myUrl.query.search_query} are >>>`);
+        break;
 
       default:
-        res.end("it 404 error\n"+myUrl.query.name+"  "+myUrl.query.id);
+        res.end("it 404 error\n" + myUrl.query.name + "  " + myUrl.query.id);
         break;
     }
   });
-});
+}
+const myServer = http.createServer(myHandler);
 
 myServer.listen(PORT, () => console.log(`server started at PORT=${PORT}`));
